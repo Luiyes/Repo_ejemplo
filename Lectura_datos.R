c08c458ecb1ebe18ef5a08c536e4bdaf8f47a195
Luiyes <luiyes1xd@gmail.com>
2022-02-19 11:30:07 -0500

    Lectura de base de datos

---
+++ Lectura_datos.R
@@ -0,0 +1,5 @@
+
+library(foreign)
+WVS <- read.dta("https://github.com/aquijanoruiz/elquantificador_posts/raw/master/valores/2020-11-02-los-ecuatorianos-aprueban-el-aborto/WV6_Data_Ecuador_2013_Stata_v20180912.dta")
+
+table(WVS$V203A)
